77261f62aa4de9d46b86c02cf6bc52d02290b82d
Alex Saveliev <lyolik@codeminders.com>
2016-06-21 13:10:19 +0300

    corrections

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -1,19 +1,18 @@
-(function() {
-	// expression bodies
-    [].map((v, i) => v + i);
+// expression bodies
+[].map((v, i) => v + i);
 
 
-    // statement bodies
-    [].forEach(v => {
-        v++;
-    });
+// statement bodies
+[].forEach(v => {
+    v++;
+});
 
-    // lexical this
-    class lexicalthis {
-        constructor() {
-            this.c = 0;
-            setInterval(() => {
-                this.c++;}, 1000);
-        }
+// lexical this
+class lexicalthis {
+    constructor() {
+        this.c = 0;
+        setInterval(() => {
+            this.c++;
+        }, 1000);
     }
-})();
+}
